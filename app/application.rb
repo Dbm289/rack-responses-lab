59180c451cc2e72bd2d1424c5d34d93f18ef1b59
Dan <DanielMooreRealPerson@gmail.com>
2021-02-13 22:52:38 -0500

    commit tree 7e7311f68734afbd666a455b04b5a4f366102a8a

--- app/application.rb
+++ app/application.rb
@@ -1,22 +1,38 @@
-class Application
+#class Application
 
-    def call(env)
-        resp = Rack::Response.new
+ #   def call(env)
+  #      resp = Rack::Response.new
 
-        time = Time.now
+   #     now = Time.now 
 
-        if time.hour < 1200
-            resp.write "Good Morning!"
-        else
-            resp.write "Good Afternoon!"
-        end
+    #    if now.hour < 1200
+    #        resp.write "Good Morning"
+     #   else
+   #         resp.write "Good Afternoon"
+      #  end
 
-        resp.finish
+       # resp.finish
 
 
         
 
+    #end
+
+
+#end
+
+class Application
+
+    def call(env)
+      resp = Rack::Response.new
+      now = Time.now
+      
+      if now.hour >=12
+        resp.write "Good Afternoon"
+      else
+        resp.write "Good Morning"
+      end
+      resp.finish
     end
-
-
-end
+  
+  end
